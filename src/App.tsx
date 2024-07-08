f33300fe247531e6feb968806be11dc7b7409d88
frerot <ntwalifrerotjacques@gmail.com>
2024-07-08 12:30:37 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <section className='bg-black text-white h-screen w-screen flex items-center justify-center'>
+    <p>Locus Program. Coming Soon...</p>
+   </section>
+  );
+}
+
+export default App
